36f400e01ff67fdf6edd1ec8f1ac305e32194c78
CinPer <damien.dorcis@yahoo.com>
2020-07-10 09:09:56 +0000

    Initial commit

--- src/Log.php
+++ src/Log.php
@@ -1,55 +1,124 @@
 <?php
+
+/*
+ * This file is part of the Notify package.
+ *
+ * (c) Prince Dorcis <princedorcis@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Prinx\Notify;
 
+/**
+ * Default logger.
+ *
+ * Simply log into the application.
+ */
 class Log
 {
     protected $file = '';
     protected $cache = '';
+    protected $levels = [
+        'debug',
+        'info',
+        'notice',
+        'warning',
+        'error',
+        'critical',
+        'alert',
+        'emergency',
+    ];
 
     public function __construct($file = '', $cache = '')
     {
-        $this->file = $file ?: realpath(__DIR__ . '../../') . '/app.log';
-        $this->cache = $cache ?: '.' . $file . '.cache';
+        // File will be created on the first logging attempt
+        $this->file = $file;
+        $this->cache = $cache ?: ".{$file}-logcount.cache";
 
         if (!file_exists($this->cache)) {
             file_put_contents($this->cache, 0);
         }
     }
 
-    public function info($message, $flag = FILE_APPEND)
-    {
-        $num = intval(file_get_contents($this->cache)) + 1;
-
-        $to_log = is_string($message) ? trim($message) : print_r($message, true);
-        $to_log = '#' . $num . ' [' . date("D, d m Y, H:i:s") . "]\n" . $to_log . "\n\n";
-
-        file_put_contents($this->file, $to_log, $flag);
-        file_put_contents($this->cache, $num);
-    }
-
-    public function warn($message, $flag = FILE_APPEND)
-    {
-        $this->info($message, $flag);
-    }
-
-    public function error($message, $flag = FILE_APPEND)
-    {
-        $this->info($message, $flag);
-    }
-
-    public function alert($message, $flag = FILE_APPEND)
-    {
-        $this->info($message, $flag);
-    }
-
     public function debug($message, $flag = FILE_APPEND)
     {
-        $this->info($message, $flag);
+        $this->log('debug', $message, $flag);
+    }
+
+    public function info($message, $flag = FILE_APPEND)
+    {
+        $this->log('info', $message, $flag);
     }
 
     public function notice($message, $flag = FILE_APPEND)
     {
-        $this->info($message, $flag);
+        $this->log('notice', $message, $flag);
+    }
+
+    public function warning($message, $flag = FILE_APPEND)
+    {
+        $this->log('warning', $message, $flag);
+    }
+
+    public function error($message, $flag = FILE_APPEND)
+    {
+        $this->log('error', $message, $flag);
+    }
+
+    public function critical($message, $flag = FILE_APPEND)
+    {
+        $this->log('critical', $message, $flag);
+    }
+
+    public function alert($message, $flag = FILE_APPEND)
+    {
+        $this->log('alert', $message, $flag);
+    }
+
+    public function emergency($message, $flag = FILE_APPEND)
+    {
+        $this->log('emergency', $message, $flag);
+    }
+
+    /**
+     * Log method for any level
+     *
+     * If $message is string, it will be logged as-is.
+     * If message is an array, it will be converted to json
+     * If message is an object AND implements the `__toString` method
+     * it will be converted to string
+     * Else, the message will be print with print_r.
+     *
+     * @param string $level
+     * @param string|array|object $message
+     * @param const $flag
+     * @return void
+     * @throws \Exception
+     */
+    public function log(string $level, $message, $flag = FILE_APPEND)
+    {
+        if (!method_exists($this, $level)) {
+            throw new \Exception('Log level `' . $level . '` not supported');
+        }
+
+        if (is_string($message)) {
+            $toLog = trim($message);
+        } elseif (is_array($message)) {
+            $toLog = json_encode($message, JSON_PRETTY_PRINT);
+        } elseif (is_object($message) && method_exists($message, '__toString')) {
+            $toLog = strval($message);
+        } else {
+            $toLog = print_r($message, true);
+        }
+
+        $num = intval(file_get_contents($this->cache)) + 1;
+
+        $toLog = '#' . $num . ' [' . strtoupper($level) . '] [' . date("D, d m Y, H:i:s") . "]\n" . $toLog . "\n\n";
+
+        file_put_contents($this->file, $toLog, $flag);
+        file_put_contents($this->cache, $num);
     }
 
     public function clear()
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @todo The tests need works
+ */
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(1 === 1);
+    }
+}
